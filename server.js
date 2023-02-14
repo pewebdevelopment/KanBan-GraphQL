92727714f9d4dc982533fe3e0c967ff161a43871
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-14 13:04:25 +0530

    Connected with GraphQL

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require("express");
 
 const connectMongo = require("./utils/connection");
 
-const { ApolloServer, gql } = require("apollo-server");
-
+const { ApolloServer } = require("apollo-server");
 const Resolvers = require("./Resolvers/index");
 
 const TypeDefinitions = require("./GraphQLSchema/index");
